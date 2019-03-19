a7437d293006a209fc66221da3bd4171b6988de0
aminos <amri.amine@protonmail.com>
2019-03-20 00:04:46 +0100

    added main program and some fixes

--- ch2/2.1-4.py
+++ ch2/2.1-4.py
@@ -1,10 +1,10 @@
-#!/bin/env python3 
+#!/bin/env python3
 
 
 def sum(a, b):
     c = [0] * (len(a) + 1)
     carry = 0
-    for i in range(0, len(a), -1):
+    for i in range(len(a) - 1, -1, -1):
         if a[i] == 1 and b[i] == 1:
             c[i] = carry
             carry = 1
@@ -17,4 +17,11 @@ def sum(a, b):
         else:
             c[i] = 1
         c[0] = carry
-        return c
+    return c
+
+
+if __name__ == "__main__":
+    a = [1, 0, 1]
+    b = [0, 0, 1]
+
+    print(sum(a, b))
