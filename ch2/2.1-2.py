59ff731e15cfe25ec74868c37afbbe92954c6c6a
aminos <amri.amine@protonmail.com>
2019-03-19 20:21:06 +0100

    reverse selection sort

---
+++ ch2/2.1-2.py
@@ -0,0 +1,36 @@
+#!/bin/env python3
+
+
+import sys
+
+
+def revsort(a):
+
+    n = len(a)
+    for i in range(n):
+        key = a[i]
+        j = i - 1
+
+        while (j >= 0 and a[j] < key):
+            a[j + 1] = a[j]
+            j -= 1
+
+        a[j] = key
+
+
+if __name__ == "__main__":
+
+    n = int(input("n = "))
+
+    if (n <= 0):
+        sys.exit(1)
+
+    a = []
+    for i in range(n):
+        a.append(int(input("a[{}] = ".format(i))))
+
+    print("Before sorting:")
+    print(a)
+    revsort(a)
+    print("After sorting:")
+    print(a)
