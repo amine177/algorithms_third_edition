2e61b0751754024987541a05053f938e7607e09c
aminos <amri.amine@protonmail.com>
2019-03-19 19:38:02 +0100

    the insertion sort algorithm

---
+++ ch2/insertion_sort.py
@@ -0,0 +1,32 @@
+#!/bin/env python3
+
+import sys
+
+
+def sort(a):
+
+    n = len(a)
+    for i in range(1, n):
+        key = a[i]
+        j = i - 1
+        while (j >= 0 and a[j] > key):
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+
+
+if __name__ == "__main__":
+
+    n = int(input("n = "))
+    if (n < 0):
+        sys.exit(1)
+
+    a = []
+    for i in range(n):
+        a.append(int(input("a[{}] = ".format(i))))
+
+    print("Before sorting:")
+    print(a)
+    sort(a)
+    print("After sorting:")
+    print(a)
