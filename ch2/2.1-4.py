a89cc48481bb41979a11bc8cf1b8524a2087e3d2
aminos <amri.amine@protonmail.com>
2019-03-20 19:51:02 +0100

    fixed off by one error

--- ch2/2.1-4.py
+++ ch2/2.1-4.py
@@ -6,22 +6,22 @@ def sum(a, b):
     carry = 0
     for i in range(len(a) - 1, -1, -1):
         if a[i] == 1 and b[i] == 1:
-            c[i] = carry
+            c[i + 1] = carry
             carry = 1
         elif carry == 1 and (a[i] == 1 or b[i] == 1):
-            c[i] = 0
+            c[i + 1] = 0
             carry = 1
         elif a[i] == 0 and b[i] == 0:
-            c[i] = carry
+            c[i + 1] = carry
             carry = 0
         else:
-            c[i] = 1
-        c[0] = carry
+            c[i + 1] = 1
+    c[0] = carry
     return c
 
 
 if __name__ == "__main__":
-    a = [1, 0, 1]
+    a = [1, 1, 1]
     b = [0, 0, 1]
 
     print(sum(a, b))
