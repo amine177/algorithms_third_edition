bfc01566cdeeaf02c464f98919a62678a5896674
aminos <amri.amine@protonmail.com>
2019-03-23 21:44:17 +0100

    a better algorithm

--- ch2/2.1-4.py
+++ ch2/2.1-4.py
@@ -5,23 +5,17 @@ def sum(a, b):
     c = [0] * (len(a) + 1)
     carry = 0
     for i in range(len(a) - 1, -1, -1):
-        if a[i] == 1 and b[i] == 1:
-            c[i + 1] = carry
+        c[i + 1] = (a[i] + b[i] + carry) % 2
+        if (a[i] + b[i] + carry) > 1:
             carry = 1
-        elif carry == 1 and (a[i] == 1 or b[i] == 1):
-            c[i + 1] = 0
-            carry = 1
-        elif a[i] == 0 and b[i] == 0:
-            c[i + 1] = carry
-            carry = 0
         else:
-            c[i + 1] = 1
+            carry = 0
     c[0] = carry
     return c
 
 
 if __name__ == "__main__":
-    a = [1, 1, 1]
+    a = [1, 0, 1]
     b = [0, 0, 1]
 
     print(sum(a, b))
