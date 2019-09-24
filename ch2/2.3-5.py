60bec92a2a632e516e48e514a1120d52cd402229
aminos <amri.amine@protonmail.com>
2019-09-24 10:24:15 +0100

    made the algorithm more memory efficient

--- ch2/2.3-5.py
+++ ch2/2.3-5.py
@@ -1,17 +1,14 @@
-def search(a, e):
-    if len(a) == 0:
-        return False
-    if len(a) == 1:
-        if a[0] == e:
-            return True
-        if a[0] != e:
+def search(a, e, begin, end):
+    if begin == end:
+        if a[end] != e:
             return False
-    if a[len(a)//2] > e:
-        return search(a[0 : len(a) // 2 + 1], e)
-    elif a[len(a)//2] < e:
-        return search(a[len(a) // 2 : len(a)], e)
-    else:
-        return True
+        else:
+            return True
+    middle = (begin + end) // 2
+    if a[middle] > e:
+        return search(a, e, begin, middle)
+    elif a[middle] < e:
+        return search(a, e, middle, end)
 
 if __name__ == "__main__":
-    print(search([1, 2, 3], 2))
+    print(search([1, 2, 3], -1, 0, 2))
